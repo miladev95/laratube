f32e3f4739ed10ca5e6e37e60ff13334b4a5277e
Milad <miladev95@gmail.com>
2023-07-23 11:10:28 +0330

    error 405 customized with json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,13 @@
 
 namespace App\Exceptions;
 
+use App\Http\Controllers\Traits\Response;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use Response;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +28,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, \Exception|Throwable $exception)
+    {
+        // Handle 405 Method Not Allowed error
+        if ($exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException) {
+            return $this->errorResponse(message: 'Method Not Allowed',code: 405);
+        }
+
+        return parent::render($request, $exception);
     }
 }
